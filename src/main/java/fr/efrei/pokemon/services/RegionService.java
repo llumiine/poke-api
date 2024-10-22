d28fa846584578e7d73a93816b1f5e234f0482cd
LLumine <laamahmed06@gmail.com>
2024-10-22 15:14:11 +0200

    modification 1

--- src/main/java/fr/efrei/pokemon/services/RegionService.java
+++ src/main/java/fr/efrei/pokemon/services/RegionService.java
@@ -17,49 +17,33 @@ public class RegionService {
         this.regionRepository = regionRepository;
     }
 
-    // Trajet de la donnée
-    // Base de données -> Entité -> Repository -> Service -> Controller
     public List<Region> findAll() {
-        // SELECT * FROM pokemon;
         return regionRepository.findAll();
     }
 
     public Region findById(String id) {
-        // Optional : soit l'objet soit null
-        // SELECT * FROM pokemon WHERE id = :id
         return regionRepository.findById(id).orElse(null);
     }
 
-    // Trajet de la donnée
-    // Controller -> Service -> Repository -> Entité -> BDD
     public void save(Region pokemon) {
-        // INSERT INTO pokemon VALUES (:name, :level, :type);
         regionRepository.save(pokemon);
     }
 
     public void delete(String id) {
-        // DELETE FROM pokemon WHERE id = :id
         regionRepository.deleteById(id);
     }
 
     public void update(String id, Region regionBody) {
         Region regionAModifier = findById(id);
-        regionAModifier.setType(regionBody.getType());
         regionAModifier.setName(regionBody.getName());
-        regionAModifier.setLevel(regionBody.getLevel());
         regionRepository.save(regionAModifier);
     }
 
-    public void partialUpdate(String id, Pokemon pokemonBody) {
+    public void partialUpdate(String id, Region regionBody) {
         Region regionAModifier = findById(id);
-        if(regionBody.getType() != null) {
-            regionAModifier.setType(pokemonBody.getType());
-        }
+
         if(regionBody.getName() != null) {
             regionAModifier.setName(regionBody.getName());
-        }
-        if(pokemonBody.getLevel() != 0) {
-            regionAModifier.setLevel(regionBody.getLevel());
         }
         regionRepository.save(regionAModifier);
     }
